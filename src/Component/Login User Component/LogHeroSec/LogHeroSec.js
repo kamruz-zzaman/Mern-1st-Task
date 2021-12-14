ee03c4ecc2ec4ff116eab3d589f73ad2e052f4f9
kamruz-zaman8991 <70217992+kamruz-zaman8991@users.noreply.github.com>
2021-12-14 13:07:54 +0600

    top hed sec mobile res back join group btn adde

--- src/Component/Login User Component/LogHeroSec/LogHeroSec.js
+++ src/Component/Login User Component/LogHeroSec/LogHeroSec.js
@@ -4,6 +4,7 @@ import '../../HeroSection/HeroSection.css'
 import back from '../../../Images/HeroSec/Rectangle 3.png'
 import mobileHero from '../../../Images/HeroSec/mobile-head.png'
 import mobileHeroShadow from '../../../Images/HeroSec/mobile-head-shadow.png'
+import './LogHeroSec.css'
 const LogHeroSec = () => {
     return (
         <div >
@@ -30,6 +31,12 @@ const LogHeroSec = () => {
                     </div>
                     <div className='position-relative'>
                         <img className='w-100' src={mobileHeroShadow} alt="" />
+                    </div>
+                </div>
+                <div className='position-absolute  top-0 mt-3'>
+                    <div className=' d-flex justify-content-between ms-1'>
+                        <p className='margin-right'><i class="fas fa-arrow-left text-light fw-bold"></i></p>
+                        <button className='btn btn-transparent border ms-5 text-light'>Join Group</button>
                     </div>
                 </div>
                 <div className=''>
